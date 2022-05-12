63ce594217a120749ade3d659a4e7903a706e3f3
SpoorthyNandakumar <97216815+SpoorthyNandakumar@users.noreply.github.com>
2022-05-12 16:29:08 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -20,27 +20,5 @@ resource "aws_instance" "EC2-1" {
   }
 }
 
-resource "aws_instance" "EC2-2" {
-  ami           = var.ami-Practice2
-  instance_type = var.instance_type
-  availability_zone = var.availability_zone-Practice2
-  #user_data = file("./IIS_installation-2.ps1")
-  #user_data = file("${path.module}/IIS_installation-2.ps1")
-
-  /*user_data = <<EOF
-               <powershell>
-               Install-WindowsFeature -name Web-Server -IncludeManagementTools
-               New-Item -Path C:\inetpub\wwwroot\index.html -ItemType File -Value "RESPONSE FROM SERVER 2" -Force
-               </powershell>
-               EOF*/
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.SG-2.id]
-  subnet_id              = aws_subnet.public-subnet-2.id
-  tags = {
-    Name  = "2"
-    
-  }
-}
-
 
 
