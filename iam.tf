197fa2e89613c676519028fe03304d3bfb86b04b
SpoorthyNandakumar <97216815+SpoorthyNandakumar@users.noreply.github.com>
2022-05-13 15:52:36 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,50 +1,97 @@
 resource "aws_iam_role" "test_role" {
   name = "test_role"
-
-  assume_role_policy = <<EOF
+assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-    "Statement": {
-    "Effect": "Allow",
-    "Action": "iam:CreateUser",
-    "Resource": "*"
-  }
-    
-    
-    {
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Action": "iam:*AccessKey*",
-    "Resource": "arn:aws:iam::792820380616:user/$aws:username"
-  }
-}
-  ]
-}
-EOF
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:GetInstanceProfile",
+                "iam:GetRole",
+                "iam:GetRolePolicy",
+                "iam:ListAttachedRolePolicies",
+                "iam:ListRolePolicies",
+                "iam:PassRole"
+            ],
+            "Resource": [
+                "arn:aws:iam::792820380616:role/test_role",
+                "arn:aws:iam::792820380616 :role/test_role"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AuthorizeSecurityGroupEgress",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:CancelSpotInstanceRequests",
+                "ec2:CreateNetworkInterface",
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateTags",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DeleteSecurityGroup",
+                "ec2:DeleteTags",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeAccountAttributes",
+                "ec2:DescribeDhcpOptions",
+                "ec2:DescribeImages",
+                "ec2:DescribeInstanceStatus",
+                "ec2:DescribeInstances",
+                "ec2:DescribeKeyPairs",
+                "ec2:DescribeLaunchTemplates",
+                "ec2:DescribeNetworkAcls",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DescribePrefixLists",
+                "ec2:DescribeRouteTables",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSpotInstanceRequests",
+                "ec2:DescribeSpotPriceHistory",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeTags",
+                "ec2:DescribeVpcAttribute",
+                "ec2:DescribeVpcEndpoints",
+                "ec2:DescribeVpcEndpointServices",
+                "ec2:DescribeVpcs",
+                "ec2:DetachNetworkInterface",
+                "ec2:ModifyImageAttribute",
+                "ec2:ModifyInstanceAttribute",
+                "ec2:RequestSpotInstances",
+                "ec2:RevokeSecurityGroupEgress",
+                "ec2:RunInstances",
+                "ec2:TerminateInstances",
+                "ec2:DescribeVolumeStatus",
+                "ec2:DescribeVolumes",
+                "elasticmapreduce:TerminateJobFlows",
+                "elasticmapreduce:ListSteps",
+                "elasticmapreduce:ListClusters",
+                "elasticmapreduce:RunJobFlow",
+                "elasticmapreduce:DescribeCluster",
+                "elasticmapreduce:AddTags",
+                "elasticmapreduce:RemoveTags",
+                "elasticmapreduce:ListInstanceGroups",
+                "elasticmapreduce:ModifyInstanceGroups",
+                "elasticmapreduce:GetCluster",
+                "elasticmapreduce:DescribeStep",
+                "elasticmapreduce:AddJobFlowSteps",
+                "elasticmapreduce:ListInstances",
+                "iam:ListInstanceProfiles",
+                "redshift:DescribeClusters"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+      EOF
 
   tags = {
       tag-key = "tag-value"
   }
 }
-
-
-
+     
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test_profile"
   role = "${aws_iam_role.test_role.name}"
 }
-
-
 
 resource "aws_iam_role_policy" "test_policy" {
   name = "test_policy"
@@ -65,6 +112,51 @@ resource "aws_iam_role_policy" "test_policy" {
 }
 EOF
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
