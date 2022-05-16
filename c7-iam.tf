7751313539846a9b94b8f3c64cbeae073daf788d
SpoorthyNandakumar <97216815+SpoorthyNandakumar@users.noreply.github.com>
2022-05-16 11:03:06 +0530

    Update c7-iam.tf

--- c7-iam.tf
+++ c7-iam.tf
@@ -1,79 +1,43 @@
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = "${aws_iam_role.test_role.id}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role" "test_role" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+    ]
+  })
 }
-EOF
-
-  tags = {
-      tag-key = "tag-value"
-  }
-}
-
-
 
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test_profile"
   role = "${aws_iam_role.test_role.name}"
 }
 
-
-
-resource "aws_iam_role_policy" "test_policy" {
-  name = "test_policy"
-  role = "${aws_iam_role.test_role.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy" "test_policy_1" {
-  name = "test_policy_1"
-  role = "${aws_iam_role.test_role.id}"
-policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "iam:*",
-      "Resource": "*",
-      "Condition": {
-        "StringEquals": {
-          "aws:username": "admin"
-        },
-        "IpAddress": {
-          "aws:SourceIp": [
-            "1.1.1.0/24",
-            "2.2.2.0/24"
-          ]
-        }
-      }
-    }
-  ]
-}
-EOF
-}
