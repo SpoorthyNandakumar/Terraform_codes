f1468d6cf49e25be3e6de85259d327863e204eaf
SpoorthyNandakumar <97216815+SpoorthyNandakumar@users.noreply.github.com>
2022-05-16 10:17:48 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -3,17 +3,15 @@ resource "aws_iam_role" "test_role" {
 
   assume_role_policy = <<EOF
  {
-"Version": "2012-10-17",
+  "Version": "2012-10-17",
   "Statement": [
     {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
       "Effect": "Allow",
-      "Action": "iam:*",
-      "Resource": "*",
-      "Condition": {
-        "StringEquals": {
-          "aws:username": "admin"
-        },
-         }
+      "Sid": ""
     }
   ]
 }
@@ -38,15 +36,18 @@ resource "aws_iam_role_policy" "test_policy" {
   role = "${aws_iam_role.test_role.id}"
 
   policy = <<EOF
-{
-  "Version": "2012-10-17",
+ {
+"Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [
-        "s3:*"
-      ],
       "Effect": "Allow",
-      "Resource": "*"
+      "Action": "iam:*",
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "aws:username": "admin"
+        },
+         }
     }
   ]
 }
