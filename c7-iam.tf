94da2777bcdb3e37bc2e0837d5ccf50a2788d7a0
SpoorthyNandakumar <97216815+SpoorthyNandakumar@users.noreply.github.com>
2022-05-17 10:46:51 +0530

    Update c7-iam.tf

--- c7-iam.tf
+++ c7-iam.tf
@@ -1,21 +1,4 @@
-resource "aws_iam_role" "sample_1" {
-  name = "sample_1"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-          role_arn = "arn:aws:iam::792820380616:role/sample_1"
-        }
-      },
-    ]
-  })
-}
+iam_role = var.aws_iam_role
 
 
 resource "aws_iam_role_policy" "test_policy" {
