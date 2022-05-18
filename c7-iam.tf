3438c752ce8809862b8a78e6dddfc1cdde77bb30
SpoorthyNandakumar <97216815+SpoorthyNandakumar@users.noreply.github.com>
2022-05-18 12:31:41 +0530

    Update c7-iam.tf

--- c7-iam.tf
+++ c7-iam.tf
@@ -16,8 +16,8 @@ resource "aws_iam_role" "test_role" {
         "sts:AssumeRole"
         ],
       "Principal": {
-        type        :"Service"
-        identifiers : ["ec2.amazonaws.com"]
+        "type"        :"Service"
+        "identifiers" : ["ec2.amazonaws.com"]
       },
      
       "Resource":"*"
